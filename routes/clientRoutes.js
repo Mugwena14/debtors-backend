57e51f76c7a2a5b6423a035a8939a5a5f1c1df48
Makhubele Langavi <mlangaviclyde@gmail.com>
2026-01-10 21:15:48 +0200

    Routes Fix

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { getClients, addClient } from '../controllers/clientController.js';
+import { getClients, addClient, updateClient, deleteClient} from '../controllers/clientController.js';
 
 // Route: GET /api/clients
 router.get('/', getClients);
